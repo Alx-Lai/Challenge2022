ac4eaf388151c0e5c5d9fbd5cfd40d85e4f6980b
張佑亘 <b10902021@ntu.edu.tw>
2022-06-10 19:46:54 +0800

    add key,pos,color constants

--- Const.py
+++ Const.py
@@ -3,7 +3,8 @@ import pygame as pg
 # model
 FPS = 60 # frame per second
 GAME_LENGTH = 30 * FPS
-PLAYER_INIT_POSITION = [pg.Vector2(200, 400), pg.Vector2(600, 400)]
+PLAYER_INIT_POSITION = [pg.Vector2(200, 400), pg.Vector2(600, 400), pg.Vector2(200, 0), pg.Vector2(200, 0)]
+
 PLAYER_RADIUS = 75
 SPEED_ATTACK = 100
 SPEED_DEFENSE = 70
@@ -28,17 +29,47 @@ WINDOW_CAPTION = 'Challenge 2020 Homework'
 WINDOW_SIZE = (800, 800)
 ARENA_SIZE = (800, 800)
 BACKGROUND_COLOR = pg.Color('black')
-PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta')]
+PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta'), pg.Color('yellow'), pg.Color('blue')]
 
 
 # controller
-PLAYER_KEYS = {
-    pg.K_UP: (1, 'up'),
-    pg.K_LEFT: (1, 'left'),
-    pg.K_DOWN: (1, 'down'),
-    pg.K_RIGHT: (1, 'right'),
-    pg.K_w: (0, 'up'),
-    pg.K_a: (0, 'left'),
-    pg.K_s: (0, 'down'),
-    pg.K_d: (0, 'right'),
+
+PLAYER_MOVE_FRONT = 1
+PLAYER_MOVE_BACK = -1
+PLAYER_ROTATE_LEFT = 1
+PLAYER_ROTATE_RIGHT = -1
+
+PLAYER_MOVE_KEYS = {
+    pg.K_w: (0, PLAYER_MOVE_FRONT),
+    pg.K_s: (0, PLAYER_MOVE_BACK),
+    pg.K_t: (1, PLAYER_MOVE_FRONT),
+    pg.K_g: (1, PLAYER_MOVE_BACK),
+    pg.K_i: (2, PLAYER_MOVE_FRONT),
+    pg.K_k: (2, PLAYER_MOVE_BACK),
+    pg.K_UP: (3, PLAYER_MOVE_FRONT),
+    pg.K_DOWN: (3, PLAYER_MOVE_BACK)
 }
+
+
+PLAYER_ROTATE_KEYS = {
+    pg.K_a: (0, PLAYER_ROTATE_LEFT),
+    pg.K_d: (0, PLAYER_ROTATE_RIGHT),
+    pg.K_f: (1, PLAYER_ROTATE_LEFT),
+    pg.K_h: (1, PLAYER_ROTATE_RIGHT),
+    pg.K_j: (2, PLAYER_ROTATE_LEFT),
+    pg.K_l: (2, PLAYER_ROTATE_RIGHT),
+    pg.K_LEFT: (3, PLAYER_ROTATE_LEFT),
+    pg.K_RIGHT: (3, PLAYER_ROTATE_RIGHT)
+}
+
+PLAYER_ATTACK_KEYS = {
+    pg.K_q: (0, 'attack'),
+    pg.K_r: (1, 'attack'),
+    pg.K_u: (2, 'attack'),
+    pg.K_RSHIFT: (3, 'attack')
+}
+
+GAME_STOP_KEY = pg.K_SPACE
+GAME_CONTINUE_KEY = pg.K_RETURN
+GAME_RESTART_KEY = pg.K_F5
+GAME_FULLSCREEN_KEY = pg.K_F11
