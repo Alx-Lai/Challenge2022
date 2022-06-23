375ac150f9eb4359dd6ad3cd2e2408f405f86442
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2022-06-23 12:56:04 +0800

    add: re field and respawning mechanism

--- Const.py
+++ Const.py
@@ -38,6 +38,13 @@ BULLET_TRACE_TIME = 0.1 * FPS  # T
 PLAYER_RESPAWN_TIME = 5 * FPS             # T
 PLAYER_MAX_RESPAWN_COUNT = [2, 2, 2, 2]
 
+PLAYER_QUOTA_ATTACK_CD = 5
+PLAYER_QUOTA_REPULSION = 5
+PLAYER_QUOTA_AUX_LINE_LENGTH = math.inf
+
+PLAYER_RESPAWN_TIME = 5 * FPS             # T
+PLAYER_MAX_RESPAWN_COUNT = [2, 2, 2, 2]
+
 BULLET_RADIUS = 0.2                       # L
 BULLET_REPULSION = 30 / FPS               # L / T
 BULLET_SPEED = 15 / FPS                   # L / T
@@ -91,6 +98,11 @@ RE_FIELD_RADIUS = 0.5                     # L
 OBSTACLE_POSITION = [pg.Vector2(i + 0.5, j + 0.5) for i in range(13, 17) for j in range(13, 17)]
 RE_FIELD_POSITION = [pg.Vector2(i + 0.5, j + 0.5) for i in range(13, 17) for j in range(17, 19)]
 
+OBSTACLE_RADIUS = 0.5                     # L
+RE_FIELD_RADIUS = 0.5                     # L
+OBSTACLE_POSITION = [pg.Vector2(i + 0.5, j + 0.5) for i in range(13, 17) for j in range(13, 17)]
+RE_FIELD_POSITION = [pg.Vector2(i + 0.5, j + 0.5) for i in range(13, 17) for j in range(17, 19)]
+
 # view
 WINDOW_CAPTION = 'Challenge 2022'
 WINDOW_SIZE = (900, 900)
@@ -118,7 +130,8 @@ ARENA_SIZE = (800, 800)
 ARENA_GRID_COUNT = 30
 ARENA_GRID_SIZE = ARENA_SIZE[0] / ARENA_GRID_COUNT
 BACKGROUND_COLOR = pg.Color('black')
-PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta'), pg.Color('yellow'), pg.Color('blue')]
+PLAYER_COLOR = [pg.Color(0, 255, 0), pg.Color(255, 0, 255), pg.Color(255, 255, 0), pg.Color(0, 0, 255)]
+PLAYER_COLOR_RESPAWN = [pg.Color(0, 127, 0), pg.Color(127, 0, 127), pg.Color(127, 127, 0), pg.Color(0, 0, 127)]
 
 PLAYER_PIC = {
     'G_BASIC':'green_basic.png',
