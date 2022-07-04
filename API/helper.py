3aedb056d53bd4fcf63e659a9aca0e7bfc1410a5
Sean <seanhuang1228@gmail.com>
2022-07-04 20:35:28 +0800

    feat: init API dir and init helper class

---
+++ API/helper.py
@@ -0,0 +1,63 @@
+import pygame as pg
+import Const
+
+class Helper(object):
+    def __init__(self, model, index):
+        self.model = model
+        self.player_id = index
+
+    # get game data
+
+    # get personal data
+    
+    # general
+    def get_players_id(self):
+
+    def get_players_hp(self):
+
+    def get_players_score(self):
+
+    # movement
+    def get_players_pos(self):
+
+    # attack
+    def get_players_kick(self):
+
+    def get_players_repulsion(self):
+
+    def get_players_len(self):
+
+    def get_players_shoot_cd(self):
+
+    def get_players_shoot_len(self):
+
+    # status
+
+    # get all player data
+
+    # general
+    def get_allplayers_pos(self):
+
+    def get_allplayers_score(self):
+
+    # movement
+
+    # attack
+
+    # status
+
+    # get specific player data
+
+    # movement
+
+    # attack
+
+    # status
+
+    # get item data
+    def get_items_pos(self):
+
+    # get field data
+    def get_wall_pos(self):
+
+    def get_RE_field_pos(self):
