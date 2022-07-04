cf1f25e6f1305cef6e444f73b410a9c409cdf70f
Sean <seanhuang1228@gmail.com>
2022-07-04 20:41:00 +0800

    fix: add pass in unimplemented functions

--- API/helper.py
+++ API/helper.py
@@ -12,24 +12,33 @@ class Helper(object):
     
     # general
     def get_players_id(self):
+        pass
 
     def get_players_hp(self):
+        pass
 
     def get_players_score(self):
+        pass
 
     # movement
     def get_players_pos(self):
+        pass
 
     # attack
     def get_players_kick(self):
+        pass
 
     def get_players_repulsion(self):
+        pass
 
     def get_players_len(self):
+        pass
 
     def get_players_shoot_cd(self):
+        pass
 
     def get_players_shoot_len(self):
+        pass
 
     # status
 
@@ -37,8 +46,10 @@ class Helper(object):
 
     # general
     def get_allplayers_pos(self):
+        pass
 
     def get_allplayers_score(self):
+        pass
 
     # movement
 
@@ -56,8 +67,11 @@ class Helper(object):
 
     # get item data
     def get_items_pos(self):
+        pass
 
     # get field data
     def get_wall_pos(self):
+        pass
 
     def get_RE_field_pos(self):
+        pass
