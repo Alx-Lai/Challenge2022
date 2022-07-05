190e8035c158c092c3c918a6ca3000c1ee2d6e41
Ccucumber12 <chris20021112@gmail.com>
2022-07-05 21:51:53 +0800

    add: 3 APIs related to player attack
    - get_player_next_attack()
    - get_player_attack_range()
    - get_player_aux_length()

--- API/helper.py
+++ API/helper.py
@@ -43,17 +43,20 @@ class Helper(object):
     def get_player_attack_cd(self):
         pass
 
-    def get_player_next_attack(self):
-        pass
+    def get_player_next_attack(self) -> list:
+        """return the remaining time (frames) until every player's next attck, use id as index"""
+        return [player.gun.cd_time for player in self.model.players]
 
-    def get_players_attack_range(self):
-        pass
+    def get_player_attack_range(self) -> list:
+        """return the attack range of every player, use id as index"""
+        return [player.bullet_trace_time * player.gun.bullet_trace_time_multiplier for player in self.model.players]
 
     def get_player_attack_accuracy(self):
         pass
 
-    def get_player_aux_length(self):
-        pass
+    def get_player_aux_length(self) -> list:
+        """return the auxiliary line length of every player, use id as index"""
+        return [player.aux_line_length for player in self.model.players]
 
     def get_player_gun_type(self):
         pass
