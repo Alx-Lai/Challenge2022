d2278034e54f2b027409975f71877d20e0262a9b
Ccucumber12 <chris20021112@gmail.com>
2022-07-07 00:21:30 +0800

    add: several APIs related to buff info

--- API/helper.py
+++ API/helper.py
@@ -44,19 +44,32 @@ class Helper(object):
         pass
 
     def get_player_next_attack(self) -> list:
-        """return the remaining time (frames) until every player's next attck, use id as index"""
+        """return the remaining time until every player's next attack in frames, use id as index."""
         return [player.gun.cd_time for player in self.model.players]
+    
+    def get_player_attack_speed(self) -> list:
+        """return the attack speed of every play in shots per second, use id as index."""
+        return [player.attack_speed * player.gun.attack_speed_multiplier for player in self.model.players]
 
     def get_player_attack_range(self) -> list:
-        """return the attack range of every player, use id as index"""
-        return [player.bullet_trace_time * player.gun.bullet_trace_time_multiplier for player in self.model.players]
+        """return the attack range of every player in grids, use id as index."""
+        return [player.bullet_lifespan * player.gun.bullet_lifespan_multiplier * Const.BULLET_SPEED for player in self.model.players]
 
-    def get_player_attack_accuracy(self):
-        pass
+    def get_player_attack_accuracy(self) -> list:
+        """return the attack accuracy of every player in radian, use id as index."""
+        return [player.attack_accuracy * player.gun.bullet_accuracy_multiplier for player in self.model.players]
 
-    def get_player_aux_length(self) -> list:
-        """return the auxiliary line length of every player, use id as index"""
-        return [player.aux_line_length for player in self.model.players]
+    def get_player_basic_attack_speed(self) -> list:
+        """return the basic attack speed without multiplier from gun of every play in shots per second, use id as index."""
+        return [player.attack_speed for player in self.model.players]
+
+    def get_player_basic_attack_range(self) -> list:
+        """return the basic attack range without multiplier from gun of every player in grids, use id as index."""
+        return [player.bullet_lifespan * Const.BULLET_SPEED for player in self.model.players]
+
+    def get_player_basic_attack_accuracy(self) -> list:
+        """return the basic attack accuracy without multiplier from gun of every player in radian, use id as index."""
+        return [player.attack_accuracy for player in self.model.players]
 
     def get_player_gun_type(self):
         pass
@@ -67,6 +80,38 @@ class Helper(object):
     def get_bullet_info(self):
         pass
 
+    # get buff info
+    def get_player_buff_count(self) -> list:
+        """return the amount of buffs on every player."""
+        return [
+            [{
+                "type": Const.BUFF_TYPE_ATTACK_SPEED,
+                "count": Const.PLAYER_QUOTA_ATTACK_SPEED - player.quota_attack_speed
+            }, {
+                "type": Const.BUFF_TYPE_REPULSION,
+                "count": Const.PLAYER_QUOTA_REPULSION - player.quota_repulsion
+            }, {
+                "type": Const.BUFF_TYPE_ATTACK_ACCURACY,
+                "count": Const.PLAYER_QUOTA_ATTACK_ACCURACY - player.quota_attack_accuracy
+            }
+            ] for player in self.model.players
+        ]
+
+    def get_player_specific_buff_count(self, buff_type: int) -> list:
+        """return the amount of specific buff on every player."""
+        ret = []
+        for buff_dict in self.get_player_buff_count():
+            for buff_info in buff_dict:
+                if buff_info["type"] == buff_type:
+                    ret.append(buff_info["count"])
+        return ret 
+    
+    def get_specific_player_buff_count(self, player_id: int) -> list:
+        """return the amount of buffs on specific player."""
+        if player_id not in range(Const.PLAYER_NUMBER):
+            return []
+        else:
+            return self.get_player_buff_count()[player_id]
 
     # get item data
     def get_item_info(self):
