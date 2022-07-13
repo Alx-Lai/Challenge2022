c55d994f893f06933f7a437353aacad5acfae4cc
Ccucumber12 <chris20021112@gmail.com>
2022-07-13 17:26:45 +0800

    add: enhance in interface

--- API/interface.py
+++ API/interface.py
@@ -122,6 +122,10 @@ class Interface(object):
                 traceback.print_exc()
                 player.player_name, player.is_AI = "Error", False
                 continue
+            try:
+                player.enhance(self.player_AI[player.player_id].enhancement)
+            except:
+                self.load_msg(str(player.player_id), player.player_name, "Cannot load enhancement")    
             self.load_msg(str(player.player_id), player.player_name, "Successful to Load")
 
     def load_msg(self, player_id, player_name ,msg):
