c23fc38e4d29396f099dbf9049816ffb80720169
Ccucumber12 <chris20021112@gmail.com>
2022-07-14 12:19:12 +0800

    fix: obstacle position clip

--- Model/GameObject/obstacle.py
+++ Model/GameObject/obstacle.py
@@ -10,10 +10,10 @@ class Obstacle(Base_Square_Object):
     def __init__(self, model, position, radius):
         super().__init__(model, position, radius)
     
-    '''
-    Clip an collided object's position to avoid overlapping
-    '''
     def clip_object_position(self, obj):
+        '''
+        Clip an collided object's position to avoid overlapping
+        '''
         if not isinstance(obj, Base_Circle_Object): return None
         if not self.collide_object(obj): return None
 
@@ -22,10 +22,10 @@ class Obstacle(Base_Square_Object):
         if abs(dx) < self.radius or abs(dy) < self.radius:
             if abs(dx) > abs(dy):
                 obj.x = self.x + (self.radius + obj.radius) * ((dx>0)-(dx<0))
-                return Vector2(1, 0)
+                return Vector2((dx>0)-(dx<0), 0)
             else:
                 obj.y = self.y + (self.radius + obj.radius) * ((dy>0)-(dy<0))
-                return Vector2(0, 1)
+                return Vector2(0, (dy>0)-(dy<0))
         else:
             corner = self.position + self.radius * Vector2((dx>0)-(dx<0), (dy>0)-(dy<0))
             direction = obj.position - corner
